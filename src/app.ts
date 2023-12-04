65073df1332a37a63ad53d1051c5c8a90ac5f54d
Abir Tasrif <abirtasrif@gmail.com>
2023-12-04 22:24:27 +0600

    updated app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
-import mongoSanitize from 'express-mongo-sanitize';
+import ExpressMongoSanitize from 'express-mongo-sanitize';
 import helmet from 'helmet';
 import hpp from 'hpp';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 // MAIN FILE
 class App {
@@ -25,11 +27,33 @@ class App {
     );
     this.app.use(cors());
     this.app.use(morgan('dev'));
-    this.app.use(mongoSanitize);
+    this.app.use(ExpressMongoSanitize);
     this.app.use(helmet());
     this.app.use(hpp());
   }
 
-  private setUpRoutes(): void {}
-  private connectToDatabase(): void {}
+  private setUpRoutes(): void {
+    this.app.get('/', (req: Request, res: Response) => {
+      res.status(200).json({ message: 'Welcome to Fushion FIx Server!' });
+    });
+  }
+  private connectToDatabase(): void {
+    const URI = process.env.MONGO_URI as string;
+
+    mongoose
+      .connect(URI)
+      .then(() => {
+        const PORT = process.env.PORT || 4000;
+
+        this.app.listen(PORT, () => {
+          console.log(`Server is running on port: ${PORT}`);
+        });
+      })
+      .catch((error) => {
+        console.log(`MongoDB connection error: ${error}`);
+      });
+  }
 }
+
+dotenv.config();
+new App();
