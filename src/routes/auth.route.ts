f04c03a494b913c09d8651c609f7d0ae344b529b
Abir Tasrif <abirtasrif@gmail.com>
2023-12-08 01:34:02 +0600

    ok register and bug in login

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 import express from 'express';
+import AuthController from '../controllers/auth.controller';
 
 const authRouter: Router = express.Router();
 
+const authInstance = new AuthController();
+
 //Register Route
-authRouter.post('/register');
+authRouter.post('/register', authInstance.register);
 
 //Login Route
-authRouter.post('/login');
+authRouter.post('/login', authInstance.login);
 
 export default authRouter;
