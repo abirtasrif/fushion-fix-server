a42fe88c4329504f049398bfe78f1abbad915bc8
Abir Tasrif <abirtasrif@gmail.com>
2023-12-09 19:40:44 +0600

    updated controllers

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,17 +1,35 @@
 import express, { Router } from 'express';
+import AuthMiddleware from '../middlewares/auth.middleware';
+import UserController from '../controllers/user.controller';
 
 const userRouter: Router = express.Router();
 
-//get all users
-userRouter.get('/');
+const authInstance = new AuthMiddleware();
+const userInstance = new UserController();
 
 //get a single user
-userRouter.get('/:uid');
+userRouter.get('/:uid', authInstance.isAuthenticated, userInstance.getAnUser);
 
 //delete a single user
-userRouter.delete('/:uid');
+userRouter.delete(
+  '/:uid',
+  authInstance.isAuthenticated,
+  userInstance.deleteAnUser
+);
 
 //update a single user
-userRouter.put('/:uid');
+userRouter.put(
+  '/:uid',
+  authInstance.isAuthenticated,
+  userInstance.UpdateAnUser
+);
+
+//get all users
+userRouter.get(
+  '/',
+  authInstance.isAuthenticated,
+  authInstance.isAdmin,
+  userInstance.getAllUsers
+);
 
 export default userRouter;
